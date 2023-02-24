1c94548a7ac121a0b75d6b1a4db02c35f012b167
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-02-24 14:38:07 +0900

    ClassList.add

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,14 @@
-console.log(parseInt("Hello"));
+const body = document.body;
+
+function color() {
+  body.classList.remove("beige");
+  if (window.innerWidth <= 1920 && window.innerWidth >= 1000) {
+    body.className = "";
+    body.classList.add("yellow");
+  } else {
+    body.className = "";
+    body.classList.add("aqua");
+  }
+}
+
+window.addEventListener("resize", color);
